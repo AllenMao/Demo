5b7352297ff829998cc60c99d431db573aeeba8a
Aley Mao <mlhbmy@163.com>
2017-05-19 10:45:40 +0800

    Update solve.py
    correct acc-loss

--- CIFAR10_TRAINING/solve.py
+++ CIFAR10_TRAINING/solve.py
@@ -17,25 +17,31 @@ caffe.set_mode_gpu()
 solver = caffe.SGDSolver('solver2.prototxt')
 
 max_iter = 10000
-test_iter = 100
-train_acc = np.zeros(int(np.ceil(max_iter / test_iter)))
-train_loss = np.zeros(int(np.ceil(max_iter / test_iter)))
-
-for i in range(max_iter):#25*4000
+test_iter = 100	#test_size / batch_size
+test_interval = 100	#test times
+display = 50
+test_acc = np.zeros(int(np.ceil(max_iter * 1.0 / test_interval)))
+train_loss = np.zeros(int(np.ceil(max_iter * 1.0 / display)))
+loss = 0; acc = 0
+for i in range(max_iter):
     solver.step(1)
-
-    if i % 20 == 0:
-        print 'Iteration', i, '......loss:', solver.net.blobs['loss'].data
-    if i % test_iter == 0:
-    	solver.test_nets[0].forward(start = 'conv1')
-        acc = solver.test_nets[0].blobs['accuracy'].data#accuracy
+    loss += solver.net.blobs['loss'].data	#compute batch_size images in every iter.
+    if i % display == 0:	#display
+        print 'Iteration', i, '......loss:', loss / display
+        train_loss[i / display] = loss / display
+        loss = 0
+    if i % test_interval == 0:
+        acc = 0
+        for j in range(test_iter):
+            solver.test_nets[0].forward()
+            acc += solver.test_nets[0].blobs['accuracy'].data#accuracy
+        acc /= test_iter 
         print 'Iteration', i, 'testing... accuracy:', acc
-        train_loss[i / test_iter] = solver.net.blobs['loss'].data
-        train_acc[i / test_iter] = acc
+        test_acc[i / test_interval] = acc
 _, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax1.plot(np.arange(int(np.ceil(max_iter / test_iter))), train_loss,'b')
-ax2.plot(np.arange(int(np.ceil(max_iter / test_iter))), train_acc,'r')
+ax1.plot(display * np.arange(len(train_loss)), train_loss,'b')
+ax2.plot(test_interval * np.arange(len(test_acc)), test_acc,'r')
 ax1.set_xlabel('iter')
 ax1.set_ylabel('loss')
 ax2.set_ylabel('acc')
